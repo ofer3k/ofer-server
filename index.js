8f51629fce4dbbfee373e8081f4cb57c4f69acd5
ofer3k <ofer3klein@gmail.com>
2021-11-01 13:46:29 +0200

    another rout

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const port = process.env.PORT || 3000;
 app.get("/", (req, res) => {
   res.send("hello");
 });
+app.get("/data", (req, res) => {
+  res.send({ name: "ofer" });
+});
 
 app.listen(port, () => {
   console.log(`running on ${port}`);
